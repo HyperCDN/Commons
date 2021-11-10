2e15f343d8cab88fef5552d9c26f2332128b3f88
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2021-11-11 00:39:38 +0100

    added rate limiter

---
+++ src/main/java/de/hypercdn/commons/api/ratelimiter/RateLimiter.java
@@ -0,0 +1,29 @@
+package de.hypercdn.commons.api.ratelimiter;
+
+import de.hypercdn.commons.imp.ratelimiter.RateLimiterException;
+
+import java.util.concurrent.TimeUnit;
+
+public interface RateLimiter {
+
+    TimeUnit getRefillUnit();
+
+    long getRefillInterval();
+
+    int getUnderflowFactor();
+
+    RateLimiter setUnderflowFactor(int factor);
+
+    long getMaximum();
+
+    RateLimiter setMaximum(long max);
+
+    long getRemaining();
+
+    long getRefillTimestamp();
+
+    void drainBy(long amount) throws RateLimiterException;
+
+    boolean tryDrainBy(long amount);
+
+}
