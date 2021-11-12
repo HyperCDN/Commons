4c127fd005276749dc65676a45107257f004e9fd
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2021-11-12 20:34:27 +0100

    added LockUtil

---
+++ src/main/java/de/hypercdn/commons/imp/lock/LockUtil.java
@@ -0,0 +1,34 @@
+package de.hypercdn.commons.imp.lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockUtil {
+
+    private LockUtil(){}
+
+    public static void tryLockInTime(Lock lock){
+        tryLockInTime(lock, 10, TimeUnit.SECONDS);
+    }
+
+    public static void tryLockInTime(Lock lock, long time, TimeUnit timeUnit){
+        try {
+            if (!lock.tryLock() && !lock.tryLock(time, timeUnit))
+                throw new IllegalStateException("Failed to acquire lock within "+time+" "+timeUnit.name());
+        }catch(InterruptedException e){
+            throw new IllegalStateException("Unable to acquire lock while thread is interrupted!");
+        }
+    }
+
+    public static void executeLocked(ReentrantLock lock, Runnable runnable){
+        try{
+            tryLockInTime(lock);
+            runnable.run();
+        }finally{
+            if(lock.isHeldByCurrentThread())
+                lock.unlock();
+        }
+    }
+
+}
