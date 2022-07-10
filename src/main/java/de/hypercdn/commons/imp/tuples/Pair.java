c9af2c9cb28b7ca8271625f9ccd155d7f973a6c0
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2022-07-11 00:30:44 +0200

    updated tuples to records

--- src/main/java/de/hypercdn/commons/imp/tuples/Pair.java
+++ src/main/java/de/hypercdn/commons/imp/tuples/Pair.java
@@ -3,25 +3,8 @@ package de.hypercdn.commons.imp.tuples;
 /**
  * Tuple representing a pair
  *
- * @param <V1> type
- * @param <V2> type
+ * @param <A> type
+ * @param <B> type
  */
-public class Pair<V1, V2>{
-
-	private final V1 v1;
-	private final V2 v2;
-
-	public Pair(V1 v1, V2 v2){
-		this.v1 = v1;
-		this.v2 = v2;
-	}
-
-	public V1 getValue1(){
-		return v1;
-	}
-
-	public V2 getValue2(){
-		return v2;
-	}
-
+public record Pair<A, B>(A a, B b){
 }
--- src/main/java/de/hypercdn/commons/imp/tuples/Triple.java
+++ src/main/java/de/hypercdn/commons/imp/tuples/Triple.java
@@ -3,32 +3,9 @@ package de.hypercdn.commons.imp.tuples;
 /**
  * Tuple representing a triple
  *
- * @param <V1> type
- * @param <V2> type
- * @param <V3> type
+ * @param <A> type
+ * @param <B> type
+ * @param <C> type
  */
-public class Triple<V1, V2, V3>{
-
-	private final V1 v1;
-	private final V2 v2;
-	private final V3 v3;
-
-	public Triple(V1 v1, V2 v2, V3 v3){
-		this.v1 = v1;
-		this.v2 = v2;
-		this.v3 = v3;
-	}
-
-	public V1 getValue1(){
-		return v1;
-	}
-
-	public V2 getValue2(){
-		return v2;
-	}
-
-	public V3 getValue3(){
-		return v3;
-	}
-
+public record Triple<A, B, C>(A a, B b, C c){
 }
